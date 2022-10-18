3071742a7d0de9da07a9ccd9053c0219814935d1
Prudhvi Raj <prudhvi@123.com>
2022-10-18 22:05:41 +0530

    logout

--- src/Pages/Welcome.js
+++ src/Pages/Welcome.js
@@ -1,16 +1,26 @@
-import { NavLink, Outlet } from "react-router-dom";
+import { NavLink, Outlet, useNavigate } from "react-router-dom";
 import { verifyEmail } from "../API/api";
 
 function Welcome() {
 
-  async function verifyEmailHandler() {
+  const navigate = useNavigate()
+
+  async function verifyEmailHandler(e) {
+    e.preventDefault()
     await verifyEmail()
+  }
+
+  function logoutHandler(e) {
+    e.preventDefault()
+    localStorage.removeItem('user@mail.com')
+    navigate('/login')
   }
 
   return (
     <div>
       <p>Welcome to Expense Tracker</p>
       <button onClick={verifyEmailHandler} >Verify Email</button>
+      <button onClick={logoutHandler} >Logout</button>
       <div>
         <p>Your profile is incomplete.<NavLink to='profile'>Complete now</NavLink></p>
       </div>
